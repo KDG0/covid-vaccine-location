b22ed8ff292ab00f019599f0d9706a8a28a483af
Kevin Guifarro <49283504+KDG0@users.noreply.github.com>
2022-07-10 16:03:27 -0600

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+    <L-map v-model="zoom"
+      v-model:zoom="zoom"
+      :center="[47.41322, -1.219482]">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      ></l-tile-layer>
+      <l-marker :lat-lng="[47.41322, -1.219482]">
+        <l-icon :icon-url="icon" :icon-size="[56, 56]" />
+      </l-marker>
+    </L-map>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LIcon,
+} from '@vue-leaflet/vue-leaflet';
+import { ref } from 'vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LIcon,
+  },
+  setup() {
+    const zoom = ref(7);
+    return {
+      zoom,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.home {
+  width: 100%;
+  height: 100%;
+}
+</style>
